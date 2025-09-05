8f2028a93a0b04862c9f430dc0f6982658a1557e
genaretionstride-cmd <genaretionstride@gmail.com>
2025-09-06 02:32:59 +0700

    hellojava.js

--- hellojava.js
+++
@@ -1,7 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello Java!");
-        System.out.println("สวัสดีจากโลกของ Java");
-        System.out.println("Welcome to Java Programming!");
-    }
-}
